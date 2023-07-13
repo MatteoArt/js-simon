29f53a09c5ee2fec31e068c6513ae789280e5b67
MatteoArt <mattrim8@gmail.com>
2023-07-13 17:55:34 +0200

    setto orologio ora attuale

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 //elemento dove visualizzerò il timer
 const currentTimeEl = document.querySelector(".current-time");
 
-const currentTime = new Date();
+let currentTime = new Date();
 
 currentTimeEl.innerHTML = `${currentTime.getHours()} : ${currentTime.getMinutes()} : 
 ${currentTime.getSeconds()}`;
+
+//con la funzione setinterval vado a dare funzionamento effettivo
+//all'orologio
+
+setInterval(function () {
+    currentTime = new Date();
+    currentTimeEl.innerHTML = `${currentTime.getHours()} : ${currentTime.getMinutes()} : 
+${currentTime.getSeconds()}`;
+}, 1000); //ogni secondo viene aggiornato l'oggetto Date(), eseguita l'istruzione della funzione di callback e quindi
+          //viene aggiornata la data ogni secondo
